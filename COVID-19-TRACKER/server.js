737cd749a328d48cf1ed2a8aad0aea30149fb1c1
ravindraindia4u <ravindraindia4u@gmail.com>
2020-06-15 22:14:09 +0530

    covid-19 tracker app

---
+++ COVID-19-TRACKER/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+const novelCovid = require("novelcovid");
+const handlebars = require("express-handlebars");
+
+app.set("view engine", "handlebars");
+
+app.engine(
+  "handlebars",
+  handlebars({
+    defaultView: "home",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+
+novelCovid.countries().then((response) => {
+  console.log(response);
+});
+
+app.get("/", function (req, res) {
+  novelCovid.countries().then((response) => {
+    res.render("home", { info: response });
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000/`);
+});
